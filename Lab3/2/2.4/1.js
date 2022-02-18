8b2b98a1405c2b585f4ec61b77e475c0158cecfa
usMurager <86411871+usMurager@users.noreply.github.com>
2022-02-18 18:42:19 +0600

    Create 1.js

---
+++ Lab3/2/2.4/1.js
@@ -0,0 +1,15 @@
+// let admin, user;
+
+// user = 'Джон';
+
+// admin = user;
+
+// alert(admin);
+
+// let outplanetname = "Земля";
+
+// let username = 'Марк';
+
+// Для BIRTHDAY можно использовать, а для age нельзя, 
+// потому что в отличие от birthday, age вычисляется.
+
