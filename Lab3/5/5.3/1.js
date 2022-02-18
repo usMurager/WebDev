4dae8eae38ea05d63c331196d68c1fa15087e2e6
usMurager <86411871+usMurager@users.noreply.github.com>
2022-02-18 18:47:58 +0600

    Create 1.js

---
+++ Lab3/5/5.3/1.js
@@ -0,0 +1,24 @@
+// 1
+let newStr = str[0].toUpperCase() + str.slice(1);
+
+// 2
+function checkSpam(str) {
+  let lowerStr = str.toLowerCase();
+
+  return lowerStr.includes('viagra') || lowerStr.includes('xxx');
+}
+
+alert( checkSpam('buy ViAgRA now'));
+alert( checkSpam('free xxxxx'));
+alert( checkSpam("innocent rabbit"));
+
+// 4
+function truncate(str, maxlength) {
+  return (str.length > maxlength) ?
+    str.slice(0, maxlength - 1) + '…' : str;
+}
+
+// 5
+function extractCurrencyValue(str) {
+  return +str.slice(1);
+}
