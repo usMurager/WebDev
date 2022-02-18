c47bb17aa561ed3e038a22b61214656a56305ff0
usMurager <86411871+usMurager@users.noreply.github.com>
2022-02-18 18:47:03 +0600

    Create 1.js

---
+++ Lab3/5/5.11/1.js
@@ -0,0 +1,27 @@
+// 1
+let user = {
+  name: "John",
+  years: 30
+};
+
+let {name, years: age, isAdmin = false} = user;
+
+alert( name ); 
+alert( age ); 
+alert( isAdmin );
+
+// 2
+function topSalary(salaries) {
+
+  let max = 0;
+  let maxName = null;
+
+  for(const [name, salary] of Object.entries(salaries)) {
+    if (max < salary) {
+      max = salary;
+      maxName = name;
+    }
+  }
+
+  return maxName;
+}
