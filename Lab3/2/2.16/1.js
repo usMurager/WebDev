dc90f7ac85231d4923f8088504235b2885ab4982
usMurager <86411871+usMurager@users.noreply.github.com>
2022-02-18 18:40:20 +0600

    Create 1.js

---
+++ Lab3/2/2.16/1.js
@@ -0,0 +1,27 @@
+let age = prompt("Сколько Вам лет?", 18);
+
+let welcome;
+
+if (age < 18) {
+
+  welcome = function() {
+    alert("Привет!");
+  };
+
+} else {
+
+  welcome = function() {
+    alert("Здравствуйте!");
+  };
+
+}
+
+welcome();
+
+let age = prompt("Сколько Вам лет?", 18);
+
+let welcome = (age < 18) ?
+  function() { alert("Привет!"); } :
+  function() { alert("Здравствуйте!"); };
+
+welcome();
