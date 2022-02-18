43c173219d0f03469eded55958458e6cdd2a0340
usMurager <86411871+usMurager@users.noreply.github.com>
2022-02-18 18:38:15 +0600

    Create 1.js

---
+++ Lab3/2/2.10/1.js
@@ -0,0 +1,16 @@
+let value = prompt('Введите число', 0);
+
+if (value > 0) {
+  alert( 1 );
+} else if (value < 0) {
+  alert( -1 );
+} else {
+  alert( 0 );
+}
+
+let message = (login == 'Сотрудник') ? 'Привет' :
+  (login == 'Директор') ? 'Здравствуйте' :
+  (login == '') ? 'Нет логина' :
+  '';
+
+result = (a + b < 4) ? 'Мало' : 'Много';
